dc2558d17991e2315ad4a84fb8a5e1b945736c60
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-09-19 20:51:41 +0200

    add missing file

---
+++ Code/main.py
@@ -0,0 +1,11 @@
+def get_from_cfg(cfg_dict: dict, param=None):
+    """
+    Return item from config dictionary.
+    dictionary has to have a key "NAME" that is the name
+    of the instance to return and a key "PARAMETERS"
+    which is a dict of the required parameters.
+    """
+    if param is None:
+        return eval(cfg_dict["NAME"])(**cfg_dict["PARAMETERS"])
+    else:
+        return eval(cfg_dict["NAME"])(param, **cfg_dict["PARAMETERS"])
